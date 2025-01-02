9a00eeef5a32d42c6ee77f58c47b27affb6f9247
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-01 19:16:06 -0500

    Create Slapfarm.lua

---
+++ Slapfarm.lua
@@ -0,0 +1,40 @@
+function elgato()
+fireclickdetector(workspace.Lobby["el gato"].ClickDetector)
+wait(0.1)
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Cat"):FireServer()
+wait(0.2)
+game:GetService("ReplicatedStorage"):WaitForChild("GeneralAbility"):FireServer()
+wait(0.4)
+fireclickdetector(workspace.Lobby["Baller"].ClickDetector)
+wait(0.3)
+game:GetService("ReplicatedStorage").GeneralAbility:FireServer()
+wait(0.4)
+fireclickdetector(workspace.Lobby["Blink"].ClickDetector)
+wait(0.3)
+game:GetService("ReplicatedStorage").Blink:FireServer("OutOfBody", {["dir"] = Vector3.new(0, 0, 0),["ismoving"] = false,["mousebehavior"] = Enum.MouseBehavior.Default})
+wait(0.4)
+fireclickdetector(workspace.Lobby.Replica.ClickDetector)
+wait(0.01)
+game:GetService("ReplicatedStorage").Duplicate:FireServer(true)
+wait(0.2)
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport2, 0)
+wait(0.3)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(697, 87, -99)
+if game.Players.LocalPlayer.Character:FindFirstChild("entered") then
+for i, v in pairs(workspace.Arena.island5.Slapples:GetChildren()) do
+                if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and game.Players.LocalPlayer.Character:FindFirstChild("entered") and v.Name == "Slapple" or v.Name == "GoldenSlapple" and v:FindFirstChild("Glove") and v.Glove:FindFirstChildWhichIsA("TouchTransmitter") then
+                    v.Glove.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+                end
+            end
+        end
+task.wait()
+while true do
+for i, v in pairs(workspace:GetChildren()) do
+                if v.Name:match(game.Players.LocalPlayer.Name) and v:FindFirstChild("HumanoidRootPart") then
+game.ReplicatedStorage.b:FireServer(v:WaitForChild("HumanoidRootPart"),true)
+                end
+            end
+task.wait()
+end
+end
+coroutine.wrap(elgato)()
