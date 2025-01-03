5120efdd21c58d0977314c77d1f579a8f346d076
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-03 13:23:07 -0500

    Slap farm

---
+++ BetterSlapfarm.lua
@@ -0,0 +1,21 @@
+AutoExecute = false -- If true, the script will autoexecute himself without placing it into the auto-execute folder. (YOUR EXECUTOR MUST SUPPORT QUEUE_ON_TELEPORT)
+
+BypassAnticheat = true -- Bypass the anti-cheat enabled so no issues happen with the Anti-cheat.
+
+NoCollide = false -- don't set it to true if you don't wanna problems.
+
+GhostInvis = true -- Applying Invisibility To Player (666+ Slaps Required)
+
+Safespot = true -- It takes you to Safespot before entering the Arena so people don't see the El Gato replica and stuff.
+
+FarmOrbs = true -- Farms every orb in-game
+
+FarmToolbox = true -- Farm toolbox (very useless)
+
+SlappleFarm = true -- Farms All Slapples
+
+Ballerfarm = false -- Gives you barely 2-10 extra slaps it bad. 
+
+BlinkFarm = false -- This gives a lot of slaps with replica Disabled since people will most likely not have Blink.
+
+Replicafarm = true -- spawns a replica for the farm.
