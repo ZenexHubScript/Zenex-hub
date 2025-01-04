d868a20026e0f212033d3ebe94f999c5c6496f7d
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-04 15:03:06 -0500

    Added every feature for config

--- BetterSlapfarm.lua
+++ BetterSlapfarm.lua
@@ -1,12 +1,26 @@
-AutoExecute = false -- If true, the script will autoexecute himself without placing it into the auto-execute folder. (YOUR EXECUTOR MUST SUPPORT QUEUE_ON_TELEPORT)
+-- Auto execute removed for causing issues
 
 BypassAnticheat = true -- Bypass the anti-cheat enabled so no issues happen with the Anti-cheat.
 
+AntiRecord = true -- It puts common English words and kicks if someone puts the keyword.
+
+GhostInvis = true -- Applying Invisibility To Player (666 Slaps required)
+
 NoCollide = false -- don't set it to true if you don't wanna problems.
 
-GhostInvis = true -- Applying Invisibility To Player (666+ Slaps Required)
+Safespot = true -- It takes you to a safe spot and then starts the farm.
 
-Safespot = true -- It takes you to Safespot before entering the Arena so people don't see the El Gato replica and stuff.
+Ballerfarm = false -- Gives you barely 2-10 extra slaps it bad.
+
+BlinkFarm = false -- This gives a lot of slaps with replica Disabled since people will most likely not have Blink.
+
+Replicafarm = true -- spawns a replica for the farm.
+
+SlappowerV1 = true -- It does a normal amount of power hitting the replica blink or baller (Recommended for bad devices)
+
+SlappowerV1.5 = false -- It does more power but causes more lag and in return, you get more slaps (Recommended for a Decent device)
+
+SlappowerV2 = false -- It does a lot of power Use V1 for less lag but this does give more slaps
 
 FarmOrbs = true -- Farms every orb in-game
 
@@ -14,10 +28,6 @@ FarmToolbox = true -- Farm toolbox (very useless)
 
 SlappleFarm = true -- Farms All Slapples
 
-Ballerfarm = false -- Gives you barely 2-10 extra slaps it bad. 
+SlappleFarmV2 = true -- Keep this on to keep grabbing the slapples just in case you don't fully grab them.
 
-BlinkFarm = false -- This gives a lot of slaps with replica Disabled since people will most likely not have Blink.
-
-Replicafarm = true -- spawns a replica for the farm.
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Scaroontop/Zenex-hub/refs/heads/main/SlapfarmFeatures.lua'))()
+loadstring(game:HttpGet(''))()
