79c0eed84445de08f8eee600adacf94d8cb76d19
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 11:32:13 -0500

    Create BoxerfarmV3.lua

---
+++ Slapbattles/BoxerfarmV3.lua
@@ -0,0 +1,13 @@
+AutoExecute = false -- If true, script will autoexec himself without placing it into auto execute folder. (YOUR EXECUTOR MUST SUPPORT QUEUE_ON_TELEPORT)
+
+NoCollide = false -- don't set it true if you don't wanna problems
+
+GhostInvis = true -- Applying Invisibility To Player (666+ Slaps Required)
+
+SlappleFarm = true -- Farms All Slapples
+
+FarmToolbox = true -- Farm toolbox (very useless)
+
+FarmOrbs = true -- Farms every orb in-game
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/BoxerFarmUpgraded/main/main.lua'))()
