76dc3f43d8f1b05da58820ae16090c6b7930c76b
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 11:32:40 -0500

    Create BoxerfarmV2.lua

---
+++ Slapbattles/BoxerfarmV2.lua
@@ -0,0 +1,9 @@
+GhostInvis = true -- Applying Invisibility To Player (666+ Slaps Required)
+
+SlappleFarm = true -- Farms All Slapples
+
+FarmToolbox = true -- Farm toolbox (very useless)
+
+FarmOrbs = true -- Farms every orb in-game
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/BoxerFarmUpgraded/main/main.lua'))()
