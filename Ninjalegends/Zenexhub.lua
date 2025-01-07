58f080b45ffb28d2324c6c60adafc5b414e55a40
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 12:27:17 -0500

    Create Zenexhub.lua

---
+++ Ninjalegends/Zenexhub.lua
@@ -0,0 +1,2 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Scripts",Text = "There is only one script Executing BtProject." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150"})
+loadstring(game:HttpGet("https://rawscripts.net/raw/Ninja-Legends-BT-Project-9463", true))()
