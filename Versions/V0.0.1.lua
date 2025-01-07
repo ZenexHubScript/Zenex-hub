0bd8c1a6a2e55fe658de01bc9c40f1b37d9adf1c
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 10:29:42 -0500

    Create V0.0.1.lua

---
+++ Versions/V0.0.1.lua
@@ -0,0 +1,3 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Update message",Text = "Zenex hub is curently Updating, please wait for the update!." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150"})
+print("Update message:Zenex hub is curently Updating, please wait for the update!.")
+game:GetService("Players").LocalPlayer:Kick("Update message:Zenex hub is curently Updating, please wait for the update!.")
