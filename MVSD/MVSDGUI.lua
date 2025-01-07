58c244e97ce58c2a67dff17cd0bd2a11bc406a96
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 10:49:44 -0500

    Update MVSDGUI.lua

--- MVSD/MVSDGUI.lua
+++ MVSD/MVSDGUI.lua
@@ -1,4 +1,1 @@
-local scriptURL = 'https://raw.githubusercontent.com/Sheeshablee73/Scriptss/main/MVSD.lua'
-local response = game:HttpGet(scriptURL)
-local executeScript = loadstring(response)
-executeScript()	
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sheeshablee73/Scriptss/main/MVSD.lua"))()
