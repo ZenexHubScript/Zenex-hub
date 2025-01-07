5c9c2d5a6049d71d16ba55ec7128f1062be4f683
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 12:28:27 -0500

    Create Zenexhub.lua

---
+++ TSB/Zenexhub.lua
@@ -0,0 +1,2 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Scripts",Text = "There is only one script Executing Speed huh X." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150"})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
