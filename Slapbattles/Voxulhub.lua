71b4ba84c36ebfe0458722766a32f2ddc9293e32
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 11:28:33 -0500

    Create Voxulhub.lua

---
+++ Slapbattles/Voxulhub.lua
@@ -0,0 +1,9 @@
+local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/Voxul/VoxulHub/main/games.lua'))()
+if games[game.PlaceId] then
+  local getgenv = getgenv or getfenv
+  getgenv().VoxulLib = 'https://raw.githubusercontent.com/Voxul/VoxulHub/main/Orion.lua'
+  getgenv().VoxulWindowCONF = {Name = "Voxul Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Voxul_ORIONLIB", IntroEnabled = false, IntroText = "Voxul Hub", IntroIcon = "http://www.roblox.com/asset/?id=6035039429"}
+  loadstring(game:HttpGet(games.bURL .. games[game.PlaceId]))() 
+else 
+  warn("Voxul currently does not support this experience!")
+end
