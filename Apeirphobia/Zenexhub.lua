48228e506188045bbf7d87dde95ac35b502928b6
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 11:53:56 -0500

    Update Zenexhub.lua

--- Apeirphobia/Zenexhub.lua
+++ Apeirphobia/Zenexhub.lua
@@ -53,3 +53,4 @@ local Button = ApeirphobiaTab:CreateButton({
    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
    end,
 })
+Rayfield:LoadConfiguration()
