b15c56108d61bb133486e8f0d8caa1a01d71198f
Scaroontop <162757246+Scaroontop@users.noreply.github.com>
2025-01-07 12:26:32 -0500

    Create Zenexhub.lua

---
+++ MVSD/Zenexhub.lua
@@ -0,0 +1,2 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Scripts",Text = "There is only one script Executing MVSDGUi." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150"})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sheeshablee73/Scriptss/main/MVSD.lua"))()
