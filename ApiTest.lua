062ffa2ab733cad8c7c0545d0e20297c674bb616
Sap <162757246+Scaroontop@users.noreply.github.com>
2025-02-17 12:31:08 -0500

    Update ApiTest.lua

--- ApiTest.lua
+++ ApiTest.lua
@@ -1,2 +1,3 @@
 local function checkForUpdates()
     local version = "0.0.0"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Scaroontop/Zenexhubapi/refs/heads/main/LatestVersion.lua",true))()
